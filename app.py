fe052ffbb41b96d64aeb21e0d798fa0e5c84dc88
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-07-07 15:25:26 -0400

    completed

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ def reverse_string(string):
 
 
 # method: non-stack, using while loop
+# apprach: iterating thru the string from back to front 
+# use placeholder var of skip to keep track of how many times we need to backspace by skipping over characters
 def evaluate_keystrokes(string):
     i = len(string)-1
     result = ""
     skip = 0
 
     while i >=0:
-        print(i, string[i])
+        # print(i, string[i])
         if string[i] == "<":
             skip +=1
             i -= 1
@@ -31,15 +33,37 @@ def evaluate_keystrokes(string):
                 # print("skip is now at", skip)
                 i -= skip
                 skip = 0
-                # print("when there is skip:", "decrement i by skip of by skip # above", "then set skip to", skip)
+                # print("when there is skip:", "decrement i by skip # above to move to the appropriate next character", "then set skip to", skip)
                 # print("string[i] is now at", string[i])
             else: 
-                char = string[i]
+                # char = string[i]
                 result = string[i] + result
                 i -= 1
-                # print("when there is no skip:", "add string[i]", char, "before the existing result to become", result)
+                # print("when there is no skip:", "simply add the current character string[i]", char, "before the existing result to become", result)
+    return result
+# print(evaluate_keystrokes('abcde<fg<h'))
+
+# method: stack 
+# approach: every time we encounter <, we pop it off the stack 
+def  evaluate_keystrokes_using_stack(string):
+    stack = []
+    for char in string:
+        if char == "<":
+            # every time we encounter <, we pop it off the stack when the stack is not empty
+            if len(stack) != 0: # check for an empty stack in case < is the first character in the stack 
+                stack.pop()
+        else:
+            stack.append(char)
+        print("char:", char, "stack:", stack)
+    
+    result = ""
+    while stack:
+        # by the end, all the characters that don't get erased remain in the stack 
+        # so we simply pop them off and add them to the result string
+        result = stack.pop() + result
+        print("result:", result)
     return result
 
-print(evaluate_keystrokes('abcde<fg<h'))
+evaluate_keystrokes_using_stack('abcde<fg<h')
 
 
